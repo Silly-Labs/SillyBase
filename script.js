2c422fa932b93c3b3ff47fd316d9cf30f87f7309
HaxeFloppa <123578256+HaxeFloppa@users.noreply.github.com>
2023-04-15 13:05:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
+import { execSync } from 'child_process'
+const execSync = require('child_process').execSync;
+
 // just playing around with stuff
 var query = document.getElementByID("test").value;
+const try_out = document.createElement('button')
 
-const execSync = require('child_process').execSync;
-// import { execSync } from 'child_process';  // replace ^ if using ES modules
-
-const output = execSync('make sillybase', { encoding: 'utf-8' });  // the default is 'buffer'
-alert(output);
+try_out.innerText = 'run the code';
+try_out.addEventListener('click', () => {
+  const output = execSync('make sillybase', { encoding: 'utf-8' });
+  alert(output);
+})
