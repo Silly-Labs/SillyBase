c4c0ec67d1ec49d40c33bd39670184cda3ed9f3e
ARMathieu <100441500+ARMathieu@users.noreply.github.com>
2023-04-16 15:30:10 +0100

    added select

--- src/client.py
+++ src/client.py
@@ -21,11 +21,21 @@ insert_addr = 0
 insert_add_count = 0
 insert_str = ""
 compile_to_count = 0
+selected = 0
+selected_str = ""
+select_count = 0
 
 # lmao what are these names :skull:
-random_names = ["cosmic-cat", "silly-string", "voloptous-table", "stainless-stain", "rusted-ownership", "weakly-typed", "goober-energy"]
+random_names = ["cosmic-cat", "silly-string", "voloptous-table", "stainless-stain", "rusted-ownership", "weakly-typed", "goober-energy", "descriptive-soda", "abstractive-star", "unbiased-home", "fishy-existence", "mossy-water", "meal-rock", "fried-brick"]
 
 os.system("clear")
+
+"""
+SillyBase Client - By HaxeFloppa/epok_gamer
+"""
+
+print("SILLYBASE CLIENT --------- By HaxeFloppa/epok_gamer\n")
+print("=======================================================")
 while True:
   prompt = input("Type in file name (no format):  ")
   if prompt == "exit()":
@@ -53,8 +63,12 @@ while True:
       for letter in range(len(compile_lines[program_counter]) - 5):
         query_log_str = query_log_str + compile_lines[program_counter][query_log_count]
         query_log_count += 1
-      query_log = int(query_log_str)
-      print(queries[query_log])
+      if query_log_str == "SELECTED":
+        print(queries[selected])
+        selected = 0
+      else:
+          query_log = int(query_log_str)
+          print(queries[query_log])
       query_log = 0
       query_log_str = ""
       query_log_count = 0
@@ -76,12 +90,24 @@ while True:
       insert_add_count = 0
       insert_addr_str = ""
     elif compile_lines[program_counter] == "SAVE FILE":
-      with open(f'result/{random_names[random.randint(0,6)]}.html', 'a') as static_file:
+      with open(f'result/{random_names[random.randint(0,len(random_names) - 1)]}.html', 'a') as static_file:
+        static_file.write("<table>\n")
+        static_file.write("  <tr>\n")
         compile_to_count = 0
         while compile_to_count < len(queries):
-          static_file.write(f'<p>{queries[compile_to_count]}</p>\n')
+          static_file.write(f'    <td>{queries[compile_to_count]}</td>\n')
           compile_to_count += 1
+        static_file.write("  </tr>\n")
+        static_file.write("</table>")
         print("Queries written to static HTML.")
+    elif compile_lines[program_counter].startswith("SELECT"):
+      select_count = 7
+      for letter in range(len(compile_lines[program_counter]) - 7):
+        selected_str = selected_str + compile_lines[program_counter][select_count]
+        select_count += 1
+      selected = int(selected_str)
+      select_count = 0
+      selected_str = ""
     program_counter += 1
   compile_lines.clear()
   queries.clear()
